9c2ae56d358b32b6d393cebab7927ea506a5b9c6
vbrazas <appleitunesstore11@gmail.com>
2018-12-11 05:23:22 +0200

    v1.2

--- fillit_checker.zsh
+++ fillit_checker.zsh
@@ -1,27 +1,28 @@
 #!/bin/zsh
 
-if [[ $1 -ne "-v" && $1 -ne "-i" ]] || [[ $2 == "" || $3 == "" ]] || ! $(ls $2 $3 1>/dev/null); then
-	echo "Usage $0 (-v|-i) <1st_fillit_folder> <2nd_fillit_folder>"
-	echo
-	echo "-v:\tlaunch main valid test"
-	echo "-i:\tlaunch incorrect test"
-	exit 1;
-fi
-
 GREEN='\e[0;32m'
 RED='\e[0;31m'
 YELLOW='\e[93m'
 CYAN='\e[96m'
 MAG='\e[95m'
-BOLD='\e[1m'
 BLUE='\e[1;34m'
 RESET='\e[0m'
+
+if [[ $1 -ne "-v" && $1 -ne "-i" ]] || [[ $2 == "" || $3 == "" ]] || ! $(ls $2/Makefile $3/Makefile 1>/dev/null); then
+	echo "Usage $0 (-v|-i) <1st_fillit_folder> <2nd_fillit_folder>"
+	echo
+	echo "-v:\tlaunch main valid test"
+	echo "-i:\tlaunch incorrect test"
+	echo "${YELLOW}Be aware! Folders, that you passed as parameters, must be valid fillit project folders and contains Makefile inside."
+
+	exit 1;
+fi
 
 GEN=generator/tetri-gen
 TDIR=.fillit_tests/
 
-FST=$2
-SCD=$3
+FST=${2/.*\//}
+SCD=${3/.*\//}
 LIMITS=(1 1 26)
 
 
@@ -39,15 +40,18 @@ for i in {1..${LIMITS[1]}}; do
 	done
 done
 
-echo "${MAG}If your computer will be close to blow himself up just ^C and type in your shell the command below\n>>> kill -9 \$(pgrep fillit)"
+echo "${MAG}If your computer will be close to blow himself just press ^C and type in your shell the command below"
+echo ">>> kill -9 \$(pgrep fillit)"
+# echo "${CYAN}Press 'k' if you want to kill all 'fillit' processes immediately!"
 printf $BLUE
 while [[ $(pgrep fillit) ]]; do
 	cu=($(top -l 1 -i 1 -n 0 | awk '/CPU usage:/ {printf "%s %s\n",  $3, $5;}'))
 	CPU=$(echo ${cu[1]/\%/} + ${cu[2]/\%/} | bc)
-	echo "Waiting... for $(pgrep fillit | wc -l | sed -e 's/ //g') proccesses. CPU usage: $CPU"; sleep 5
-	if (( $CPU >= 100.7 )); then
+	echo "Waiting... for $(pgrep fillit | wc -l | sed -e 's/ //g') proccesses. CPU usage: $CPU"
+	if [[ $input == 'k' ]] || (( $CPU >= 100.7 )); then
 		kill -9 $(pgrep fillit)
 	fi
+	sleep 5
 done
 
 ERRS=0
@@ -56,7 +60,7 @@ if [[ $1 == "-v" ]]; then
 	for i in {1..${LIMITS[1]}}; do
 		for j in {${LIMITS[2]}..${LIMITS[3]}}; do
 			f="${i}_${j}"
-			if [[ "$(diff $TDIR/${FST}_${f}.out $TDIR/${SCD}_${f}.out)" ]]; then
+			if [[ "$(head $TDIR/${FST}_${f}.out)" && "$(head $TDIR/${SCD}_${f}.out)" && "$(diff $TDIR/${FST}_${f}.out $TDIR/${SCD}_${f}.out)" ]]; then
 				echo "${RED}Error occured with files: $TDIR/${FST}_${f}.out $TDIR/${SCD}_${f}.out"
 				echo "$RED----------------------------------------"
 				echo "${RESET}DUMP for ${CYAN}$TDIR/${f}.map${RESET}:\n$TDIR/${FST}_${f}.out:"
